bc55b591889262286bd683f01ca668339df9f3c0
Rachel <racmocon@gmail.com>
2019-10-10 16:54:07 -0400

    Created function for handling js bundling

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const { dest, series, src, watch } = require('gulp');
+const babel = require('rollup-plugin-babel');
 const browserSync = require('browser-sync').create();
+const rollup = require('rollup');
 
 function reload() {
   browserSync.reload();
@@ -11,8 +13,21 @@ function server() {
       baseDir: './dist'
     }
   });
-  // watch('src/**.js', { ignoreInitial: false }, compileJS);
+  watch('src/**.js', { ignoreInitial: false }, bundleJS);
   watch('dist/*.html', reload);
+}
+
+function bundleJS() {
+  return rollup.rollup({
+    input: './src/index.js',
+    plugins: [babel({ runtimeHelpers: true })]
+  }).then(bundle => {
+    return bundle.write({
+      file: './dist/main.js',
+      format: 'iife',
+      name: 'Component'
+    });
+  });
 }
 
 exports.default = server;
