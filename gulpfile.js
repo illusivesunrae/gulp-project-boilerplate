bb03e47076cad82568a2e6e1bfef07bd36e348a4
Rachel <racmocon@gmail.com>
2019-10-10 17:10:21 -0400

    Created function for adding banner to js files and created gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 const { dest, series, src, watch } = require('gulp');
 const babel = require('rollup-plugin-babel');
+const banner = require('gulp-header');
 const browserSync = require('browser-sync').create();
 const rollup = require('rollup');
+const pkg = require('./package.json');
+
+const bannerTemplate = [`/*! 
+* ${pkg.name} - @version ${pkg.version}
+
+* Copyright (C) 2018 The Trustees of Indiana University
+* SPDX-License-Identifier: BSD-3-Clause
+*/\n`];
 
 function reload() {
   browserSync.reload();
@@ -29,5 +38,13 @@ function bundleJS() {
     });
   });
 }
+
+function bannerJS() {
+  return src('./dist/*.js')
+    .pipe(banner(bannerTemplate, { pkg: pkg }))
+    .pipe(dest('./dist/'))
+}
+
+exports.build = series(bundleJS, bannerJS);
 
 exports.default = server;
